e91d881fb30546272aa9f81a54eeedea683db76e
Rafael Cavalcante <rvmascavalcante@gmail.com>
2022-10-14 01:53:02 -0300

    feat: add custom tooltip to chart

--- src/components/charts/auc-total.tsx
+++ src/components/charts/auc-total.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 import { IoIosResize } from 'react-icons/io';
-import { LineChart, Line, XAxis, Tooltip } from 'recharts';
+import { LineChart, Line, Tooltip, XAxis, TooltipProps } from 'recharts';
+import {
+  ValueType,
+  NameType,
+} from 'recharts/src/component/DefaultTooltipContent';
 
 const data = [
   {
@@ -41,7 +45,28 @@ const data = [
   },
 ];
 
+const CustomizedDot = () => {
+  return null;
+};
+
+const CustomToolTip = ({
+  active,
+  payload,
+}: TooltipProps<ValueType, NameType>) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="bg-white text-xs font-semibold flex flex-col py-1 px-2">
+        <span className="text-base-bg">R$ 148.640.700,88</span>
+        <span className="text-primary">{payload[0].value} clientes</span>
+      </div>
+    );
+  }
+  return null;
+};
+
 const DashboardChart = () => {
+  const itemsX: any = [];
+  data.map((item) => itemsX.push(item.name));
   return (
     <div className="bg-neutral h-max rounded-md py-7 pr-10 flex flex-col relative">
       <IoIosResize className="text-secondary-text text-2xl absolute right-2 top-2 cursor-pointer" />
@@ -65,12 +90,29 @@ const DashboardChart = () => {
         width={500}
         height={150}
         data={data}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        margin={{ top: 5, right: 30, left: 0, bottom: 5 }}
       >
-        <XAxis dataKey="name" />
-        <Tooltip />
-        <Line type="linear" dataKey="clientes" stroke="#D2A877" />
-        <Line type="linear" dataKey="uv" stroke="#ffffff" />
+        <XAxis
+          dataKey="name"
+          axisLine={false}
+          tickLine={false}
+          ticks={itemsX.splice(1)}
+        />
+        <Tooltip content={<CustomToolTip />} />
+        <Line
+          type="linear"
+          dataKey="clientes"
+          stroke="#D2A877"
+          dot={<CustomizedDot />}
+          activeDot={{ r: 4, strokeWidth: 0 }}
+        />
+        <Line
+          type="linear"
+          dataKey="uv"
+          stroke="#ffffff"
+          dot={<CustomizedDot />}
+          activeDot={{ r: 4, strokeWidth: 0 }}
+        />
       </LineChart>
     </div>
   );
