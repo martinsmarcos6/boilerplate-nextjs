2682118cb22c36fdb219a59345b4073bdbcc41e9
Rafael Cavalcante <rvmascavalcante@gmail.com>
2022-11-11 23:33:43 -0300

    feat: ensure menu changes color according to selected route

---
+++ src/pages/clients.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ModuleWrapper from '../layouts/ModuleWrapper';
+
+const ClientsPage = () => {
+  return <ModuleWrapper></ModuleWrapper>;
+};
+
+export default ClientsPage;
