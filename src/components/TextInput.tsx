4c77ede8482ebf5321f66648388cbddff270beba
Rafael Cavalcante <rvmascavalcante@gmail.com>
2022-12-15 18:40:27 -0300

    feat: add label to text input

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -6,19 +6,37 @@ import React, {
 
 interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
   error?: boolean;
+  variant?: 'outlined' | 'filled';
+  label?: string;
 }
 
 const TextInputBase: ForwardRefRenderFunction<
   HTMLInputElement,
   TextInputProps
-> = ({ error = false, ...rest }: TextInputProps, ref) => {
+> = (
+  { error = false, variant = 'filled', label, ...rest }: TextInputProps,
+  ref
+) => {
   const errorStyle = error && 'border-red-500';
+  const variantStyle = {
+    outlined: 'border border-neutral',
+    filled: 'bg-neutral',
+  };
+
   return (
     <div className="relative flex items-center">
       <div className="w-full max-w-[630px]">
+        {label && (
+          <label
+            htmlFor={rest.id}
+            className="text-base text-neutral font-raleway"
+          >
+            {label}
+          </label>
+        )}
         <input
           ref={ref}
-          className={`${errorStyle} input bg-neutral rounded-[5px] w-full placeholder-secondary-text text-secondary-text`}
+          className={`${errorStyle} ${variantStyle[variant]} input rounded-[5px] w-full placeholder-secondary-text text-secondary-text`}
           {...rest}
         />
       </div>
