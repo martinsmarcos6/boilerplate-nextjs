0769d668f940e98d40e2924f376a798b91acf420
Rafael Cavalcante <rvmascavalcante@gmail.com>
2022-12-15 11:47:16 -0300

    feat: add react-query to dashboard services

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,135 +1,76 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+
+import { useQuery } from 'react-query';
 
 import DashboardChart from '../components/charts/auc-total';
 import { LoadingSpinner } from '../components/LoadingSpinner';
-import CustomersOverview, {
-  ICustomerOverview,
-} from '../components/tables/CustomersOverview';
+import CustomersOverview from '../components/tables/CustomersOverview';
 import NoticesAndPendingWidget from '../components/tables/NoticesAndPendingWidget';
 import { withSSRGuest } from '../guards/withSSRGuest';
 import ModuleWrapper from '../layouts/ModuleWrapper';
 import { ChartServices } from '../services';
-import { ChartResponse } from '../services/contracts/charts';
-import { NoticesAndPendingWidgetResponse } from '../services/contracts/tables';
 import { TableServices } from '../services/table-services';
 
+const chartServices = new ChartServices();
+const tableServices = new TableServices();
 const Index = () => {
-  const chartServices = new ChartServices();
-  const tableServices = new TableServices();
-  const [loading, setLoading] = useState(true);
-  const [aucData, setAucData] = useState<ChartResponse>();
-  const [notifications, setNotifications] =
-    useState<NoticesAndPendingWidgetResponse>();
-  const [avgIncomeData, setAvgIncomeData] = useState<ChartResponse>();
-  const [customerOverviewData, setCustomerOverviewData] =
-    useState<ICustomerOverview>();
-
-  const loadChartAuc = async () => {
-    try {
-      const response = await chartServices.loadChartAUCData();
-      setAucData(response.data);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const loadChartAvgIncome = async () => {
-    try {
-      const response = await chartServices.loadChartAvgIncomeData();
-      setAvgIncomeData(response.data);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const loadNoticesAndPendingWidgetData = async () => {
-    try {
-      const response = await tableServices.loadNoticesAndPendingWidgetData();
-      setNotifications(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const loadCustomerOverviewData = async () => {
-    try {
-      const response = await tableServices.loadCustomerOverviewData();
-      setCustomerOverviewData(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const loadTables = async () => {
-    await loadCustomerOverviewData();
-    await loadNoticesAndPendingWidgetData();
-  };
-
-  const loadCharts = async () => {
-    await loadChartAvgIncome();
-    await loadChartAuc();
-  };
-
-  useEffect(() => {
-    loadCharts();
-    loadTables();
-  }, []);
-
+  const aucChart1Query = useQuery('auc-chart', () =>
+    chartServices.loadChartAUCData()
+  );
+  const avgIncomeDataChart = useQuery('avg-income-chart', () =>
+    chartServices.loadChartAvgIncomeData()
+  );
+  const noticesPendingWidgetQuery = useQuery('notices-pending-widget', () =>
+    tableServices.loadNoticesAndPendingWidgetData()
+  );
+  const customerOverviewWidgetQuery = useQuery('customer-overview-widget', () =>
+    tableServices.loadCustomerOverviewData()
+  );
   return (
     <ModuleWrapper>
-      {loading ? (
-        <main className="w-full h-full flex items-center justify-center py-9">
-          <LoadingSpinner />
-        </main>
-      ) : (
-        <main className="grid grid-cols-2 grid-rows-1 gap-5 pb-24 w-full">
-          {aucData ? (
-            <DashboardChart
-              data={aucData.data}
-              leftName={aucData.y1_name}
-              rightName={aucData.y2_name}
-              expansive
-            />
-          ) : (
-            <div className="flex items-center justify-center">
-              <LoadingSpinner />
-            </div>
-          )}
-          {avgIncomeData ? (
-            <DashboardChart
-              data={avgIncomeData.data}
-              leftName={avgIncomeData.y1_name}
-              rightName={avgIncomeData.y2_name}
-            />
-          ) : (
-            <div className="flex items-center justify-center py-28">
-              <LoadingSpinner />
-            </div>
-          )}
-          {customerOverviewData ? (
-            <CustomersOverview
-              columns={customerOverviewData?.columns}
-              data={customerOverviewData?.data}
-            />
-          ) : (
-            <div className="flex items-center justify-center py-28">
-              <LoadingSpinner />
-            </div>
-          )}
+      <main className="grid grid-cols-2 grid-rows-1 gap-5 pb-24 w-full">
+        {aucChart1Query.isSuccess ? (
+          <DashboardChart
+            data={aucChart1Query.data.data.data}
+            leftName={aucChart1Query.data.data.y1_name}
+            rightName={aucChart1Query.data.data.y1_name}
+            expansive
+          />
+        ) : (
+          <div className="flex items-center justify-center">
+            <LoadingSpinner />
+          </div>
+        )}
+        {avgIncomeDataChart.isSuccess ? (
+          <DashboardChart
+            data={avgIncomeDataChart.data.data.data}
+            leftName={avgIncomeDataChart.data.data.y1_name}
+            rightName={avgIncomeDataChart.data.data.y2_name}
+          />
+        ) : (
+          <div className="flex items-center justify-center py-28">
+            <LoadingSpinner />
+          </div>
+        )}
+        {customerOverviewWidgetQuery.isSuccess ? (
+          <CustomersOverview
+            columns={customerOverviewWidgetQuery.data.data.columns}
+            data={customerOverviewWidgetQuery.data.data.data}
+          />
+        ) : (
+          <div className="flex items-center justify-center py-28">
+            <LoadingSpinner />
+          </div>
+        )}
 
-          {notifications ? (
-            <NoticesAndPendingWidget data={notifications} />
-          ) : (
-            <div className="flex items-center justify-center py-28">
-              <LoadingSpinner />
-            </div>
-          )}
-        </main>
-      )}
+        {noticesPendingWidgetQuery.isSuccess ? (
+          <NoticesAndPendingWidget data={noticesPendingWidgetQuery.data.data} />
+        ) : (
+          <div className="flex items-center justify-center py-28">
+            <LoadingSpinner />
+          </div>
+        )}
+      </main>
     </ModuleWrapper>
   );
 };
--- src/services/chart-services.ts
+++ src/services/chart-services.ts
@@ -1,24 +1,18 @@
-import { AxiosResponse } from 'axios';
-
 import { traadApi } from '../config/api';
 import { ChartResponse } from './contracts/charts';
 
 export class ChartServices {
-  async loadChartAUCData(): Promise<AxiosResponse<ChartResponse, any>> {
-    const response = await traadApi.request<ChartResponse>({
+  async loadChartAUCData(): Promise<any> {
+    return traadApi.request<ChartResponse>({
       method: 'GET',
       url: `/v1/application/chart-one`,
     });
-
-    return response;
   }
 
-  async loadChartAvgIncomeData(): Promise<AxiosResponse<ChartResponse, any>> {
-    const response = await traadApi.request<ChartResponse>({
+  async loadChartAvgIncomeData(): Promise<any> {
+    return traadApi.request<ChartResponse>({
       method: 'GET',
       url: `/v1/application/chart-two`,
     });
-
-    return response;
   }
 }
