8b3d01a9febb0a9df85feedd9339f8f736fc26e9
Rafael Cavalcante <rvmascavalcante@gmail.com>
2022-12-16 03:24:28 -0300

    feat: ensure image size property is 'cover'

--- src/layouts/SignupWrapper.tsx
+++ src/layouts/SignupWrapper.tsx
@@ -8,7 +8,7 @@ interface SignUpWrapperProps {
 export const SignUpWrapper = ({ children }: SignUpWrapperProps) => {
   return (
     <main className="fixed left-0 top-0 bg-white w-full h-full flex">
-      <aside className="bg-[url('/assets/fractal.svg')] flex-1 bg-no-repeat flex items-center justify-center">
+      <aside className="bg-[url('/assets/fractal.svg')] flex-1 bg-no-repeat flex items-center justify-center bg-cover">
         <Logo />
       </aside>
       <aside className="flex-1 flex justify-center items-center relative">
