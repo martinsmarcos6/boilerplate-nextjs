af3a9c2acb841d464a4a347797bd50c2ca52f4b8
Rafael Cavalcante <rvmascavalcante@gmail.com>
2023-02-28 01:25:54 -0300

    feat: ensure login is responsive

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,3 @@
 export { default as Logo } from './traad-logo.svg';
+export { default as LogoDark } from './traad-dark.svg';
 export * from './lets-encrypt';
--- src/layouts/SignupWrapper.tsx
+++ src/layouts/SignupWrapper.tsx
@@ -8,10 +8,10 @@ interface SignUpWrapperProps {
 export const SignUpWrapper = ({ children }: SignUpWrapperProps) => {
   return (
     <main className="fixed left-0 top-0 bg-white w-full h-full flex">
-      <aside className="bg-[url('/assets/fractal.svg')] flex-1 bg-no-repeat flex items-center justify-center bg-cover">
+      <aside className="bg-[url('/assets/fractal.svg')] flex-1 bg-no-repeat  items-center justify-center bg-cover hidden md:flex">
         <Logo />
       </aside>
-      <aside className="flex-1 flex justify-center items-center relative">
+      <aside className="flex-1 flex justify-center items-center relative px-4">
         {children}
         <AuthFormFooter className="absolute bottom-[16px] w-full max-w-[408px]" />
       </aside>
